d8de5702ecd2522da5fff5d37bcadd373559a6e0
AflahB <aflahmohamed@gmail.com>
2015-03-19 22:33:48 +0000

    Created a new mainline into program. This class is invoked in Java given a set of arguments and produces a file with sorted elements. Currently has a main method and some comments.

---
+++ xsort.java
@@ -0,0 +1,19 @@
+public clas xsort{
+    /**
+      The main entry point of the program
+      @ param args[] The arguments passed into the program. The arguments are separated by a space. 
+     */
+    main(String args[]){
+	if(args.length > 0){
+	    // We need a way to parse the strings and run the program in the way the user has specified.
+	    // args[] contains all the arguments seperated by a space
+	    // one way to do it is to iterate through the array, parse the command check if its valid and check if the next element is an input.
+	}
+
+
+
+    }
+
+
+
+}
